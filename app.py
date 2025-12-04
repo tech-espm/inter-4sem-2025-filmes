702007f89441deac39bac8b462b1eb1ff41fa69e
MariaLuizazz <souzaluizamaria57@gmail.com>
2025-12-04 13:49:11 -0300

    querys

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, json, request, Response
 import config
 from datetime import datetime
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+
+engine = create_engine(config.conexao_banco)
 
 app = Flask(__name__)
 
@@ -19,18 +23,34 @@ def dashboard():
 
 @app.get('/obterDados')
 def obterDados():
-    dados = [
-        { 'dia': '10/09', 'valor': 80 },
-        { 'dia': '11/09', 'valor': 92 },
-        { 'dia': '12/09', 'valor': 90 },
-        { 'dia': '13/09', 'valor': 101 },
-        { 'dia': '14/09', 'valor': 105 },
-        { 'dia': '15/09', 'valor': 100 },
-        { 'dia': '16/09', 'valor': 64 },
-        { 'dia': '17/09', 'valor': 78 },
-        { 'dia': '18/09', 'valor': 93 },
-        { 'dia': '19/09', 'valor': 110 }
-    ];
+    dados = {
+        'top10': [],
+        'top10generos': [],
+        'top10anos': [],
+    }
+
+    with engine.begin() as conn:
+        registros = conn.execute(text("select f.titulo, tmp.media from (select id_filme, avg(nota) media from ranking r group by id_filme) tmp inner join filme f on f.id = tmp.id_filme order by tmp.media desc limit 10"))
+        for registro in registros:
+            dados['top10'].append({
+                'titulo': registro[0],
+                'nota': registro[1]
+            })
+
+        registros = conn.execute(text("select g.nome, tmp.media from (select id_genero, avg(nota) media from ranking r inner join genero_filme gf on gf.id_filme = r.id_filme group by gf.id_genero) tmp inner join genero g on g.id = tmp.id_genero order by tmp.media desc limit 10"))
+        for registro in registros:
+            dados['top10generos'].append({
+                'nome': registro[0],
+                'nota': registro[1]
+            })
+
+        registros = conn.execute(text("select tmp.ano, tmp.media from (select f.ano, avg(r.nota) media from ranking r inner join filme f on f.id = r.id_filme group by f.ano) tmp order by tmp.media desc limit 10"))
+        for registro in registros:
+            dados['top10anos'].append({
+                'ano': registro[0],
+                'nota': registro[1]
+            })
+
     return json.jsonify(dados)
 
 @app.post('/criar')
